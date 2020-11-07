186beaddd52dd3dc67b17169c167b436a5a5ce36
peiyu-ye <1289170688@qq.com>
2020-11-07 18:05:08 +0800

    Add public methods

---
+++ src/utils/index.ts
@@ -0,0 +1,91 @@
+
+/**
+ * Common methods 
+ * Py was created in 2020.11.07
+ */ 
+
+/**
+ * 将对象作为参数添加到URL中
+ * @param baseUrl url
+ * @param obj
+ * @returns {string}
+ * eg:
+ *  let obj = {a: '3', b: '4'}
+ *  setObjToUrlParams('www.baidu.com', obj)
+ *  ==>www.baidu.com?a=3&b=4
+ */
+export function setObjToUrlParams(baseUrl: string, obj: any): string {
+  let parameters = '';
+  let url = '';
+  for (const key in obj) {
+    parameters += key + '=' + encodeURIComponent(obj[key]) + '&';
+  }
+  parameters = parameters.replace(/&$/, '');
+  if (/\?$/.test(baseUrl)) {
+    url = baseUrl + parameters;
+  } else {
+    url = baseUrl.replace(/\/?$/, '?') + parameters;
+  }
+  return url;
+}
+
+/**
+ * 解析时间字符串
+ * @param {(Object|string|number)} time
+ * @param {string} cFormat
+ * @returns {string}
+ */
+export function parseTime(time: string | number | Date, cFormat: string = "") {
+  if (arguments.length === 0 || !time) {
+    return "";
+  }
+  const format: string = cFormat || "{y}-{m}-{d} {h}:{i}:{s}";
+  let date:any;
+  if (typeof time === "object") {
+    date = time;
+  } else {
+    if (typeof time === "string" && /^[0-9]+$/.test(time)) {
+      time = parseInt(time);
+    }
+    if (typeof time === "number" && time.toString().length === 10) {
+      time = time * 1000;
+    }
+    date = new Date(time);
+  }
+  const formatObj:any = {
+    y: date.getFullYear(),
+    m: date.getMonth() + 1,
+    d: date.getDate(),
+    h: date.getHours(),
+    i: date.getMinutes(),
+    s: date.getSeconds(),
+    a: date.getDay()
+  };
+  const time_str = format.replace(/{([ymdhisa])+}/g, (result:any, key:any) => {
+    const value:any = formatObj[key];
+    // Note: getDay() returns 0 on Sunday
+    if (key === "a") {
+      return ["日", "一", "二", "三", "四", "五", "六"][value];
+    }
+    return value.toString().padStart(2, "0");
+  });
+  return time_str;
+}
+
+/**
+ * @description: 根据数组中某个对象值去重
+ */
+export function unique<T = any>(arr: T[], key: string): T[] {
+  const map = new Map();
+  return arr.filter((item) => {
+    const _item = item as any;
+    return !map.has(_item[key]) && map.set(_item[key], 1);
+  });
+}
+
+/**
+ * @description: es6数组去重复
+ */
+export function es6Unique<T>(arr: T[]): T[] {
+  return Array.from(new Set(arr));
+}
